27f518156bd2c5c6524b6576dec530adc3e9cab2
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-07-10 10:28:55 +0700

    commit

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ window.addEventListener("load", function () {
     frameworkUrl: buildUrl + "/Build.framework.js.unityweb",
     codeUrl: buildUrl + "/Build.wasm.unityweb",
     streamingAssetsUrl: "StreamingAssets",
-    companyName: "GotoAndPlay",
+    companyName: "NGenStudio",
     productName: "Connector",
     productVersion: "1.0",
     showBanner: unityShowBanner,
