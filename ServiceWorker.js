3bfb097bba187e9be23951e14aca4ec7a575a7b0
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-08-27 17:03:56 +0700

    commit

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "CatB-Cat Battle-1.0.10.3";
+const cacheName = "CatB-Cat Battle-1.0.10.4";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
