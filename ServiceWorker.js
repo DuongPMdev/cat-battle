89a783fd3164e6d34d9b93d64a46f47e01d89d8c
duongpm13dev <duongpm13dev@gmail.com>
2024-09-29 06:18:03 +0100

    commit

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "CatB-Cat Battle-1.0.13.10";
+const cacheName = "CatB-Cat Battle-1.0.13.11";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
