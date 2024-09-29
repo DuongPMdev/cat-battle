1208a53a75eeb185f49a803221e1ea35e3651c22
duongpm13dev <duongpm13dev@gmail.com>
2024-09-29 10:55:24 +0000

    commit

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "CatB-Cat Battle-1.0.13.16";
+const cacheName = "CatB-Cat Battle-1.0.13.17";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
--- index.js
+++ index.js
@@ -93,7 +93,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "CatB",
     productName: "Cat Battle",
-    productVersion: "1.0.13.16",
+    productVersion: "1.0.13.17",
     showBanner: unityShowBanner,
 	cacheControl: function (url) {
   //return "immutable";
