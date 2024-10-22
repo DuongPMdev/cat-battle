87779b9d6c3de6e2e11696d2a6aba69e72658ae1
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-10-22 03:23:32 +0000

    commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "CatB",
     productName: "Cat Battle",
-    productVersion: "1.1.0.1",
+    productVersion: "1.1.0.2",
     showBanner: unityShowBanner,
 	cacheControl: function (url) {
   //return "immutable";
